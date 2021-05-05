3387c24b4b4f2462378b86e82707f4d7a3ec3379
alvaro santos <alvarossmiguel@gmail.com>
2021-05-05 00:51:49 -0300

    added activities

---
+++ src/components/Activities/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Feather, MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+
+import {
+  Container,
+  Header,
+  Title,
+  Card,
+  CardHeader,
+  Avatar,
+  Description,
+  Bold,
+  CardBody,
+  UserName,
+  CardFooter,
+  Details,
+  Value,
+  Divider,
+  Date,
+  Action,
+  Option,
+  OptionLabel,
+} from "./styles";
+
+import avatar from "../../images/avatar.png";
+
+export default function Activities() {
+  return (
+    <Container>
+      <Header>
+        <Title>Atividades</Title>
+      </Header>
+
+      <Card>
+        <CardHeader>
+          <Avatar source={avatar} />
+          <Description>
+            <Bold>Você</Bold> pagou a <Bold>@silviosantos</Bold>
+          </Description>
+        </CardHeader>
+
+        <CardBody>
+          <UserName>Álvaro Santos</UserName>
+        </CardBody>
+
+        <CardFooter>
+          <Details>
+            <Value>R$ 18,00</Value>
+
+            <Divider />
+
+            <Feather name="lock" color="white" size={14} />
+            <Date>Há 2 anos</Date>
+          </Details>
+
+          <Action>
+            <Option>
+              <MaterialCommunityIcons
+                name="comment-outline"
+                color="white"
+                size={14}
+              />
+              <OptionLabel>0</OptionLabel>
+            </Option>
+
+            <Option>
+              <AntDesign name="hearto" color="white" size={14} />
+              <OptionLabel>0</OptionLabel>
+            </Option>
+          </Action>
+        </CardFooter>
+      </Card>
+    </Container>
+  );
+}
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -11,6 +11,7 @@ import {
 } from "./styles";
 
 import Suggestions from "../../components/Suggestions";
+import Activities from "../../components/Activities";
 
 export default function Home() {
   return (
@@ -30,7 +31,8 @@ export default function Home() {
           <AntDesign name="gift" size={30} color="#10c86e" />
         </Header>
 
-        <Suggestions/>
+        <Suggestions />
+        <Activities />
       </Container>
     </Wrapper>
   );
