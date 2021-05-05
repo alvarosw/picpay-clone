30547b1ee608093d52fc4baaa51efa6906d7c232
alvaro santos <alvarossmiguel@gmail.com>
2021-05-04 23:54:13 -0300

    header and suggestions

--- src/components/PayButton/index.js
+++ src/components/PayButton/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Touchable, TouchableWithoutFeedback } from "react-native";
+import { TouchableWithoutFeedback } from "react-native";
 
 import { Button, Label } from "./styles";
 
@@ -10,7 +10,7 @@ export default function PayButton({ onPress, focused }) {
       <Button 
         colors={
           focused 
-            ? ["#fff","#ccc"]
+            ? ["#fff","#ccc"] 
             : ["#00fc6c", "#00ac4a"]
         } 
         start={[1, 0.2]}
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,37 @@
 import React from "react";
-import { Text } from "react-native";
+import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
 
-export default function Home(){
-    return <Text>Home</Text>
+import {
+  Wrapper,
+  Container,
+  Header,
+  Balance,
+  BalanceTitle,
+  BalanceContainer,
+} from "./styles";
+
+import Suggestions from "../../components/Suggestions";
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons
+            name="qrcode-scan"
+            size={30}
+            color="#10c86e"
+          />
+          <BalanceContainer>
+            <BalanceTitle>Meu Saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
+
+          <AntDesign name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Suggestions/>
+      </Container>
+    </Wrapper>
+  );
 }
